ff82c68471e34578bb2d3c801e1066e96b1fe046
Vorpark <nik.vorpark@mail.ru>
2024-02-17 14:49:05 +0300

    Реализация шаблонов основных контроллеров

---
+++ Pobeda_MVC/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pobeda_MVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
