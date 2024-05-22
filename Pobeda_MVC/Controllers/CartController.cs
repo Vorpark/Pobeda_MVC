23cbfcc67e738ca9448e59a4727bf65a8aa6c51f
Nikita Y. Vorobyov <nik.vorpark@gmail.com>
2024-05-22 14:52:13 +0300

    fix бага с возвращением на страничку товара и последующем крашем приложения при повторной попытке покупки

--- Pobeda_MVC/Controllers/CartController.cs
+++ Pobeda_MVC/Controllers/CartController.cs
@@ -29,9 +29,14 @@ namespace Pobeda_MVC.Controllers
         public IActionResult AddProductToCart(int productId)
         {
             var product = _unitOfWork.Product.Get(x => x.Id == productId);
-            var cart = _unitOfWork.Cart.Get(x => x.Id == 1);
+            var cart = _unitOfWork.Cart.Get(x => x.Id == 1, "Products");
             if (cart.Products == null)
                 cart.Products = new List<Product>();
+            foreach (var item in cart.Products)
+            {
+                if (item.Id == productId)
+                    return RedirectToAction("Index");
+            }
             cart.Products.Add(product);
             _unitOfWork.Cart.Update(cart);
             _unitOfWork.Save();
